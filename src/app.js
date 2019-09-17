5eebf4e5b72f9d6051384b823ccae229478be160
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-16 21:41:27 -0700

    added logger/winston

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
+const logger = require('./logger');
+//const uuid = require('uuid/v4');
 
 const app = express();
 
@@ -14,6 +16,17 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
 
 app.get('/', (req, res) =>{
     res.send('Hello, world!');
