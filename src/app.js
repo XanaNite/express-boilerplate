46f0a2a71bc41cf01372bb295edf689123a86935
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-28 15:38:29 -0700

    commented out api token req

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
-const validateBearerToken = require('./validate-bearer-token')
+//const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
 //const uuid = require('uuid/v4');
 
@@ -21,7 +21,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(validateBearerToken)
+//app.use(validateBearerToken)
 
 app.get('/', (req, res) =>{
     res.send('Hello, world!');
