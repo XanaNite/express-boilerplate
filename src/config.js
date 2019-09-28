7b73f6b963d21dfc193496dda5767f2536f8626d
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-28 14:30:46 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: process.env.API_TOKEN || '',
-    DB_URL: process.env.DB_URL || `postgresql://dunder-mifflin@localhost/{/*dbName*/}`,
+    API_TOKEN: process.env.API_TOKEN || '', //add API token
+    DB_URL: process.env.DB_URL || ``, //add database
 }
