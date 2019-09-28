6855789e5380a7d5bb635d170250dc82b50ad38e
Amber Meritt <amber.merittcampbell@gmail.com>
2019-09-28 14:24:22 -0700

    refactored boilerplate

--- src/app.js
+++ src/app.js
@@ -4,43 +4,29 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
-const logger = require('./logger');
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
 //const uuid = require('uuid/v4');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
+const morganOption = ((NODE_ENV === 'production')
     ? 'tiny'
-    : 'common';
+    : 'common',
+    {
+        skip: () => NODE_ENV === 'test'
+    })
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next){
-    const apiToken = process.env.API_TOKEN;
-    const authToken = req.get('Authorization');
-
-    if(!authToken || authToken.split(' ')[1] !== apiToken){
-        logger.error(`Unauthorized request to path: ${req.path}`);
-        return res.status(401).json({error: 'Unauthorized request'})
-    }
-    next()
-})
+app.use(validateBearerToken)
 
 app.get('/', (req, res) =>{
     res.send('Hello, world!');
 })
 
-app.use(function errorHandler(error, req, res, next){
-    let response
-    if(NODE_ENV === 'production'){
-        response = {error: {message: 'server error'}}
-    } else {
-        console.log(error);
-        response = {message: error.message, error}
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app;
--- test/app.spec.js
+++ test/app.spec.js
@@ -4,6 +4,7 @@ describe('App', () =>{
     it('GET / responds with  containing "Hello, world!"', () =>{
         return supertest(app)
             .get('/')
+            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
             .expect(200, 'Hello, world!');
     });
 });
